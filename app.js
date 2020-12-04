51bcb829591237a9700ef06f6fa770b8e27332e8
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-12-04 23:09:30 +0200

    setup environment variables and npm scripts

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { PORT, NODE_ENV } = require('./config/env');
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log(`Server started on port ${5000}`);
+app.listen(PORT, () => {
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
 });
---
+++ config/env.js
@@ -0,0 +1,7 @@
+exports.PORT = process.env.PORT || 5000;
+exports.NODE_ENV = process.env.NODE_ENV || 'development';
+
+exports.DBHOST = process.env.DBHOST || '';
+exports.DBUSER = process.env.DBUSER || '';
+exports.DBPASSWORD = process.env.DBPASSWORD || '';
+exports.DBNAME = process.env.DBNAME || '';
